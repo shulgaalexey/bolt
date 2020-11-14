94474d6cab4bcb4f88ac78556421c9323ab5595c
Alexey Shulga <alexey_shulga@rapid7.com>
2020-11-14 18:57:08 +0000

    Add some tech commands

--- bot.js
+++ bot.js
@@ -3,16 +3,43 @@ const client = new Discord.Client();
 
 
 client.on('ready', () => {
-    console.log('I am ready!');
+	console.log('Bolt is ready!');
 });
- 
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-       message.reply('pong');
+
+const cmdPrefix = '!'
+
+client.on('message', async msg => {
+	if (msg.content === 'ping') {
+		msg.reply('pong');
+		return;
     }
+
+	if(!msg.content.startsWith(cmdPrefix)) {
+		msg.reply('IDK WTF is: ' + msg.content)
+		return
+	}
+
+	// slices off prefix from our message, then trims extra whitespace,
+	// then returns our array of words from the message
+	const args = msg.content.slice(cmdPrefix.length).trim().split(' ')
+
+	//splits off the first word from the array, which will be our command
+	const command = args.shift().toLowerCase()
+
+	//log the command
+	console.log('command: ', command)
+
+	//log any arguments passed with a command
+	console.log(args)
+
+	if (command === 'hola') {
+		msg.reply('hola amigo')
+		return
+	}
+
+	msg.reply('Unknown command: ' + command)
 });
 
- 
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+
+client.login(process.env.BOT_TOKEN);
