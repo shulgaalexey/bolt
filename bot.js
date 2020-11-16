a91b316e33178e07915acef4c043062c47b29583
Alexey Shulga <alexey_shulga@rapid7.com>
2020-11-16 08:07:17 +0000

    Fix today calculation

--- bot.js
+++ bot.js
@@ -12,8 +12,7 @@ function today() {
 	const dd = String(today.getDate()).padStart(2, '0');
 	const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 	const yyyy = today.getFullYear();
-	today = yyyy + mm  + dd;
-	return today;
+	return yyyy + mm  + dd;
 }
 
 function urlifyCmd(args) {
